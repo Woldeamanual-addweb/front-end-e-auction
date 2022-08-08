697fb4294d53a1fd7ec78b04dedeffdd4c9b5454
Woldeamanual Getnet <aman.getnet2@gmail.com>
2022-08-08 16:00:00 -0400

    Add Starter

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import { Link } from "gatsby"
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1> W </h1>
+      <div class="links">
+        <Link to="/"></Link>
+        <Link to="/"></Link> <Link to="/"></Link>
+      </div>
+    </nav>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <section>
+      <div>
+      <h2>
+        Design
+      </h2>
+         <h2>
+        Develop
+      </h2>
+      </div>
+    </section>
+  
+    )
+}
